934a93296aea795747ba8ba9ba6fafafd3bc0132
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-05-10 13:04:47 +0800

    Updated module.

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "instance" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  user_data     = var.user_data
+  ami                  = var.ami
+  instance_type        = var.instance_type
+  key_name             = aws_key_pair.key_pair.key_name
+  iam_instance_profile = aws_iam_instance_profile.profile.name
+  user_data            = var.user_data
 
   network_interface {
     network_interface_id  = aws_network_interface.netif.id
---
+++ instance_profile.tf
@@ -0,0 +1,32 @@
+data "aws_iam_policy_document" "inline_policy" {
+  statement {
+    actions   = var.inline_actions
+    resources = var.inline_resources
+  }
+}
+
+resource "aws_iam_role" "role" {
+  managed_policy_arns = var.managed_policy_arns
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    policy = data.aws_iam_policy_document.inline_policy.json
+  }
+}
+
+resource "aws_iam_instance_profile" "profile" {
+  name = aws_iam_role.role.name
+  role = aws_iam_role.role.name
+}
